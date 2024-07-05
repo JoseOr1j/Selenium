61d0f75b86a2dd4f60c426fedea76bcb74bf4d08
Jose <JoseOrozco1j@gmail.com>
2024-07-04 19:27:09 -0700

    Add files via upload

--- SeleniumCSharp/AmazonTest.cs
+++ SeleniumCSharp/AmazonTest.cs
@@ -16,6 +16,8 @@ using System.CodeDom;
 using System.Data;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
+using OpenQA.Selenium.DevTools.V120.Page;
+using OpenQA.Selenium.DevTools.V120.CSS;
 [TestFixture]
 public class AmazonTest {
     private IWebDriver driver;
@@ -81,6 +83,8 @@ public class AmazonTest {
     {
         IWebDriver driver = new ChromeDriver();
 
+        
+
         driver.Navigate().GoToUrl("https://Amazon.com");
 
         driver.ToString();
@@ -91,6 +95,24 @@ public class AmazonTest {
         Assert.That(driver.Equals("https://amazon.com"), Is.True);
 
         Assert.That(driver.Url.Equals("https://amazon.com"), Is.True);
+
+        string urlName = driver.Url.ToString();
+        Console.Write(urlName);
+
+        if (urlName == "https://amazon.com")
+        {
+            Console.Write("Url name checks out: ", urlName):
+        }
+        else if (urlName == "https://amazon.co")
+        {
+            Console.Write("Mistyped, retype url");
+        }
+        else
+        {
+            Console.WriteLine("No valid url");
+        }
+
+        
 
         driver.Url = "https://amazon.com";
 
@@ -152,8 +174,71 @@ public static void clickOnItem2()
 
         driver.FindElement(By.XPath(""));
 
+        driver.FindElement(By.ClassName(pageUrl)).Click();
+
+        driver.FindElement(By.Name("title")).Click();
+
+        driver.FindElement(By.ClassName("title"));
+
 
 
         driver.Quit();
     }
+
+    [Test]
+    static void addToCart()
+    {
+        // test case for adding an item to a amazon shopping cart
+        /*
+         * P1 goes to Amazons webpage
+         * P1 searches for an item in the search box 
+         * P1 submits search
+         * P1 selects an item 
+         * P1 adds item to cart and clicks it
+         * P1 verifies that the item was added 
+         * */
+        IWebDriver driver = new ChromeDriver();
+
+        try
+        {
+            driver.Navigate().GoToUrl("https://www.amazon.com");
+
+            var searchBox = driver.FindElement(By.Id("twotabsearchtextbox"));
+            searchBox.SendKeys("Selenium WebDriver book");
+            searchBox.Submit();
+
+            // WAITS for the results to load 
+            WebDriverWait wait = new WebDriverWait(driver,TimeSpan.FromSeconds(5));
+            wait.Until(d => d.FindElement(By.CssSelector(".s-search-results .s-results-item")));
+
+            //find and click the first item in search
+            var firstResult = driver.FindElement(By.CssSelector("s-search results"));
+            // var name = driver (driver command) (by select what driver will touch);
+
+            firstResult.Click();
+
+            wait.Until(d => d.FindElement(By.Id("productTitle")));
+
+            // find and click add to cart
+            var addToCartButton = driver.FindElement(By.Id("add-to-cart-button"));
+            addToCartButton.Click();
+
+            // wait to confirm it was added 
+
+            wait.Until(d => d.FindElement(By.CssSelector(".a-size-medium-plus")));
+
+          
+        }
+
+        catch (NoSuchElementException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            driver.Quit();
+        }
+
+    }
+
 }
